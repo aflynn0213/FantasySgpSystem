7280b209dfaf2af2e54e214a60249a0400ddd383
Alex Flynn <61518876+aflynn0213@users.noreply.github.com>
2026-02-24 11:23:43 -0600

    Enhance export_sgp for combined player type
    Refactor export_sgp function to handle combined player type and fix index missing check.

--- utils/inseason_export_sgp.py
+++ utils/inseason_export_sgp.py
@@ -7,6 +7,7 @@ from utils.common_utils import upload_to_bucket, get_repo_root
 
 hitter_export_cols = ['PA', 'SGP_R', 'SGP_HR', 'SGP_RBI', 'SGP_SB', 'SGP_OBP', 'SGP_SLG', 'Total_SGP_wSB', 'Total_SGP']
 pitcher_export_cols = ['IP', 'GS', 'SGP_SO', 'SGP_QS', 'SGP_SV_HLD', 'SGP_ERA', 'SGP_WHIP', 'SGP_K/BB', 'Total_SGP']
+combined_export_cols = ['Total_SGP', 'RL', 'VAR']
 index_cols = ['Name','PlayerId']
 
     
@@ -15,10 +16,17 @@ def export_sgp(df,sb,dir,player_type):
     SAVE_FOLDER = os.path.join(get_repo_root(), "results")
     os.makedirs(SAVE_FOLDER, exist_ok=True)
     
-    export_cols = hitter_export_cols if player_type == "hitting" else pitcher_export_cols
+    if player_type == "hitting":
+        export_cols = hitter_export_cols
+    elif player_type == "pitching":
+        export_cols = pitcher_export_cols
+    elif player_type == "combined":
+        export_cols = combined_export_cols
+    else:
+        export_cols = []
     
     column_missing = [col for col in export_cols if col not in df.columns]
-    index_missing = [index for index in index_cols if index not in list(df.index.names)]
+    index_missing = [index for index in index_cols if index not in df.columns]
 
     if column_missing:
         raise ValueError(f"{column_missing} Missing From DataFrame")
