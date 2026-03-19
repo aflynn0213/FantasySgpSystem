f162a31143839eebd597deb4f5393ad8b7f2ddf4
Alex Flynn <61518876+aflynn0213@users.noreply.github.com>
2026-03-19 14:26:23 -0500

    Update preseason_entrypoint.sh (needs to refresh in order to get local copy - need to check gcs in future if nothing local for cloud runs)

--- preseason_entrypoint.sh
+++ preseason_entrypoint.sh
@@ -2,7 +2,7 @@
 set -e  # Exit immediately if any command fails
 
 echo "[*] Refreshing projections..."
-#python update_scripts/refresh_excel_projections.py
+python update_scripts/refresh_excel_projections.py
 python main.py -b atc_pre -p oopsy_pre -a atc -sb
 python main.py -b atc_pre -p oopsy_pre -a atc -m points -sb
 
