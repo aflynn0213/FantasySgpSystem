4f1f5451c93a8d52c4cfb873399eb23c5fec1bdf
Alex Flynn <61518876+aflynn0213@users.noreply.github.com>
2026-05-07 12:41:28 -0500

    Update inseason_entrypoint.sh

--- inseason_entrypoint.sh
+++ inseason_entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Running in-season job..."
-#python update_scripts/update_stats.py
+python update_scripts/update_stats.py
 python main.py -b atc_td -p atc_td -wk $(python -c "from datetime import datetime; print(min(26, (datetime.today() - datetime(2025, 3, 24)).days // 7))")
-python main.py -b batx_ros -p oopsy_ros -wk $(python -c "from datetime import datetime; print(min(26, (datetime.today() - datetime(2025, 3, 24)).days // 7))")
+#python main.py -b batx_ros -p oopsy_ros -wk $(python -c "from datetime import datetime; print(min(26, (datetime.today() - datetime(2025, 3, 24)).days // 7))")
